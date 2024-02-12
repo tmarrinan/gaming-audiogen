7463ed1b43b5b6ed6c99ac61d124ad1e7b07741b
Thomas Marrinan <thomas.j.marrinan@gmail.com>
2024-02-12 14:49:07 -0600

    two levels

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,26 @@ const BASE_URL = import.meta.env.BASE_URL || '/';
 
 let phaser = {
     canvas: null,
-    game: null
+    game: null,
+    scene: null
+}
+
+let app_state = reactive({
+    edit_mode: false
+});
+
+function onGameLoaded(data) {
+    phaser.scene = phaser.game.scene.scenes[0];
+}
+
+function selectLevel(level) {
+    if (phaser.scene !== null) {
+        phaser.scene.restart(level);
+    }
+}
+
+function editGame() {
+    app_state.edit_mode = true;
 }
 
 onMounted(() => {
@@ -21,13 +40,16 @@ onMounted(() => {
         physics: {
             default: 'arcade',
             arcade: {
-                gravity: { y: 300 },
+                gravity: { y: 981 },
                 debug: false
             }
         },
         scene: [
             GameScene
-        ]
+        ],
+        callbacks: {
+            postBoot: onGameLoaded
+        }
     };
 
     phaser.game = new Game(config);
@@ -35,7 +57,19 @@ onMounted(() => {
 </script>
 
 <template>
-    <canvas id="canvas"></canvas>
+    <div id="content">
+        <canvas id="canvas"></canvas>
+        <div class="ui" >
+            <button type="button" class="primary-btn" @click="selectLevel(0)">Level 1</button>
+            <button type="button" class="primary-btn" @click="selectLevel(1)">Level 2</button>
+            <button type="button" class="primary-btn" @click="selectLevel(2)">Level 3</button>
+            <button type="button" class="primary-btn" @click="selectLevel(3)">Level 4</button>
+            <button type="button" class="other-btn" @click="editGame">Custom</button>
+            <div v-if="app_state.edit_mode">
+                <p>EDIT</p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
@@ -43,9 +77,47 @@ onMounted(() => {
     font-size: 1rem;
 }
 
+#content {
+    width: 1050px;
+    margin: 0 auto;
+}
+
 #canvas {
     display: block;
     width: 1024px;
     height: 576px;
+}
+
+.ui {
+    text-align: center;
+    margin: 1rem;
+}
+
+.primary-btn {
+    background-color: #1A5DB5;
+    color: #FFFFFF;
+    padding:0.25rem 1rem;
+    margin: 0 1rem;
+    border: none;
+    border-radius: 0.5rem;
+    cursor: pointer;
+}
+
+.primary-btn:hover {
+    background-color: #164E96;
+}
+
+.other-btn {
+    background-color: #A9A9A9;
+    color: #000000;
+    padding: 0.25rem 1rem;
+    margin: 0 1rem;
+    border: none;
+    border-radius: 0.5rem;
+    cursor: pointer;
+}
+
+.other-btn:hover {
+    background-color: #8F8F8F;
 }
 </style>
--- src/scripts/gameScene.js
+++ src/scripts/gameScene.js
@@ -1,57 +1,212 @@
-import {Scene, GameObjects } from 'phaser';
+import { Scene } from 'phaser';
 
-class GameScene extends Scene {
-    constructor() {
-        super('scene-game');
-
-        this.canvas = null;
-        this.graphics = null;
-        this.background = [
+// Levels - note: player jump apporx 96 high
+const levels = [
+    {
+        background: [
+            0x291C61,
+            0x592880,
+            0xE37C2D
+        ],
+        platforms: [
+            // ground
+            {x: 0, y: 0, w: '100%', h: 64, color: 0x287C40},
+            // platforms
+            {x: 224, y: 160, w: 64, h: 32, color: 0xC2C2C2},
+            {x: 384, y: 192, w: 192, h: 32, color: 0xC2C2C2},
+            {x: 672, y: 128, w: 128, h: 32, color: 0xC2C2C2},
+            {x: 96, y: 192, w: 32, h: 32, color: 0xC2C2C2},
+            {x: 0, y: 288, w: 64, h: 32, color: 0xC2C2C2},
+            {x: 128, y: 384, w: 96, h: 32, color: 0xC2C2C2},
+            {x: 352, y: 384, w: 192, h: 32, color: 0xC2C2C2},
+            {x: 672, y: 416, w: 192, h: 32, color: 0xC2C2C2},
+        ],
+        goal: {x: 832, y: 480}
+    },
+    {
+        background: [
             0xACD4F2,
             0x4E8BCC,
             0x483E87
-        ];
+        ],
+        platforms: [
+            // ground
+            {x: 0, y: 0, w: '50%', h: 64, color: 0xF2E6A0},
+            {x: '80%', y: 0, w: '20%', h: 64, color: 0xF2E6A0},
+            // tree
+            {x: 225, y: 64, w: 32, h: 32, color: 0x875810},
+            {x: 225, y: 96, w: 32, h: 32, color: 0x875810},
+            {x: 225, y: 128, w: 32, h: 32, color: 0x875810},
+            {x: 161, y: 128, w: 32, h: 32, color: 0x1b7a21},
+            {x: 193, y: 160, w: 32, h: 32, color: 0x1b7a21},
+            {x: 225, y: 160, w: 32, h: 32, color: 0x1b7a21},
+            {x: 257, y: 160, w: 32, h: 32, color: 0x1b7a21},
+            {x: 289, y: 128, w: 32, h: 32, color: 0x1b7a21},
+            // cloud
+            {x: 442, y: 212, w: 141, h: 32, color: 0xFFFFFF},
+            {x: 410, y: 180, w: 205, h: 32, color: 0xFFFFFF},
+            // cloud
+            {x: 762, y: 244, w: 91, h: 32, color: 0xFFFFFF},
+            {x: 730, y: 212, w: 155, h: 32, color: 0xFFFFFF},
+            // waterfall
+            {x: 960, y: 160, w: 32, h: 256, color: 0x6BCBE8},
+            {x: 992, y: 416, w: 32, h: 32, color: 0x6BCBE8},
+            // rocks
+            {x: 992, y: 128, w: 32, h: 288, color: 0x595c57},
+            {x: 928, y: 128, w: 64, h: 32, color: 0x595c57}
+        ],
+        goal: {x: 992, y: 64}
+    }
+]
+
+class GameScene extends Scene {
+    constructor(config) {
+        super(config);
+
+        this.current_level = 0;
+
+        this.canvas = null;
+        this.cursors = null;
+        this.graphics = null;
         this.platforms = null;
         this.platform_gameobjects = [];
+        this.goal = null;
+        this.player = null;
+        this.animations = [];
+        this.textbox = null;
     }
 
     preload() {
         this.canvas = this.sys.game.canvas;
         this.load.image('platform', 'assets/platform-ninepatch.png');
+        this.load.image('goal', 'assets/goal.png');
+        this.load.spritesheet('dude', 'assets/dude.png', {frameWidth: 32, frameHeight: 48});
     }
   
     create() {
+        // Initialize keyboard input
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        // Create background graphics (gradient)
         this.graphics = this.add.graphics();
         this.redrawGradient();
 
-        let ground = this.add.nineslice(this.canvas.width / 2, this.canvas.height - 32, 'platform', 0, this.canvas.width, 64, 14, 14, 14, 14);
-        ground.tint = 0xFF0000;
-        this.platform_gameobjects.push(ground);
-        this.platform_gameobjects.push(this.add.nineslice(400, 110, 'platform', 0, 200, 32, 14, 14, 14, 14));
-        this.platform_gameobjects.push(this.add.nineslice(50, 250, 'platform', 0, 50, 32, 14, 14, 14, 14));
-        this.platform_gameobjects.push(this.add.nineslice(750, 220, 'platform', 0, 100, 32, 14, 14, 14, 14));
-
+        // Create platforms
+        levels[this.current_level].platforms.forEach((p) => {
+            let x = (typeof p.x === 'string') ? this.canvas.width * (parseFloat(p.x.substring(0, p.x.length - 1)) / 100.0) : p.x;
+            let w = (typeof p.w === 'string') ? this.canvas.width * (parseFloat(p.w.substring(0, p.w.length - 1)) / 100.0) : p.w;
+            let platform = this.add.nineslice(x + (w / 2), this.canvas.height - (p.y + (p.h / 2)), 'platform', 0, w, p.h, 14, 14, 14, 14);
+            platform.tint = p.color;
+            this.platform_gameobjects.push(platform);
+        });
 
         this.platforms = this.physics.add.staticGroup();
         this.platforms.addMultiple(this.platform_gameobjects);
+
+        // Create goal
+        let goal_pos = levels[this.current_level].goal;
+        this.goal = this.physics.add.staticImage(goal_pos.x + 16, this.canvas.height - (goal_pos.y + 32), 'goal', 0);
+
+        // Create player
+        this.player = this.physics.add.sprite(100, 475, 'dude');
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+
+        this.animations.push(this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        }));
+
+        this.animations.push(this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'dude', frame: 4 } ],
+            frameRate: 20
+        }));
+
+        this.animations.push(this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        }));
+
+        this.physics.add.collider(this.player, this.platforms);
+        this.physics.add.overlap(this.player, this.goal, this.goalReached, null, this);
+
+        // Text (win / game over)
+        const font = {
+            color: '#FFFFFF',
+            fontFamily: 'monospace',
+            fontSize: '64px'
+        };
+        this.textbox = this.add.text(this.canvas.width / 2, this.canvas.height / 2, '', font);
+        this.textbox.setOrigin(0.5, 0.5);
+        this.textbox.visible = false;
     }
 
     update(time, delta) {
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-160);
+            this.player.anims.play('left', true);
+        }
+        else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(160);
+            this.player.anims.play('right', true);
+        }
+        else {
+            this.player.setVelocityX(0);
+            this.player.anims.play('turn');
+        }
+
+        if (this.cursors.space.isDown && this.player.body.touching.down) {
+            this.player.setVelocityY(-450);
+        }
+    }
+
+    restart(level) {
+        // Set level
+        this.current_level = level;
+
+        // Clear old level assets
+        this.graphics.destroy();
+
+        for (let platform of this.platform_gameobjects) {
+            platform.destroy();
+        }
+        this.platform_gameobjects = [];
         
+        this.platforms.destroy();
+
+        for (let animation of this.animations) {
+            animation.destroy();
+        }
+        this.animations = [];
+
+        // Restart
+        this.scene.restart();
     }
 
     redrawGradient() {
         this.graphics.clear();
         let start_color, end_color, start_y;
-        let num_sections = this.background.length - 1;
+        let background = levels[this.current_level].background;
+        let num_sections = background.length - 1;
         let section_height = this.canvas.height / num_sections;
         for (let i = 0; i < num_sections; i++) {
-            start_color = this.background[i];
-            end_color = this.background[i + 1];
+            start_color = background[i];
+            end_color = background[i + 1];
             start_y = i * section_height;
             this.graphics.fillGradientStyle(start_color, start_color, end_color, end_color, 1);
             this.graphics.fillRect(0, start_y, this.canvas.width, section_height);
         }
+    }
+
+    goalReached() {
+        this.textbox.text = 'Winner!';
+        this.textbox.visible = true;
+        console.log('made it! Finshed level: ' + this.current_level);
     }
 };
 
